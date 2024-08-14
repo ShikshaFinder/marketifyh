36f8e1feafd12292c8cc9689579284fa95cbee37
ShikshaFinder <janiharsh794@gmail.com>
2024-08-14 17:45:15 +0530

    h

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,7 +84,7 @@ export default function Component() {
               </div>
             </div>
             <img
-              src="/placeholder.svg"
+              src="https://images.unsplash.com/photo-1676050552641-42d53f4c9713?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               width="650"
               height="650"
               alt="Hero"
